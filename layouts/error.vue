620feeb9eea1b6e6f6bfd7bf5b9c79cc34a064fe
Lokesh Parmar <lokesh.parmar998@gmail.com>
2021-08-28 14:53:47 +0530

    layouts added

---
+++ layouts/error.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
+  </div>
+</template>
+<script>
+export default {
+  props: ["error"],
+  layout: "fullheight" // you can set a custom layout for the error page
+};
+</script>
