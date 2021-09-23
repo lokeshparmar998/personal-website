143901f1241558efe461fa08fefc1e56afc8a00a
Lokesh Parmar <lokesh.parmar998@gmail.com>
2021-09-23 15:26:20 +0530

    C

--- components/dev-apps/qolor/QolorContainer.vue
+++ components/dev-apps/qolor/QolorContainer.vue
@@ -1,12 +1,48 @@
 <template>
   <div class="qolor-container">
     <div class="color-choices">
-      <div class="r1" :style="`background-color: ${row1}`"></div>
-      <div class="r2" :style="`background-color: ${row2}`"></div>
-      <div class="r3" :style="`background-color: ${row3}`"></div>
-      <div class="r4" :style="`background-color: ${row4}`"></div>
+      <div
+        class="r1"
+        :style="`background-color: ${row1}`"
+        @mouseenter="showR1 = true"
+        @mouseleave="showR1 = false"
+      >
+        <span class="color-hex" v-show="showR1">
+          {{ row1 }}
+        </span>
+      </div>
+      <div
+        class="r2"
+        :style="`background-color: ${row2}`"
+        @mouseenter="showR2 = true"
+        @mouseleave="showR2 = false"
+      >
+        <span class="color-hex" v-show="showR2">
+          {{ row1 }}
+        </span>
+      </div>
+      <div
+        class="r3"
+        :style="`background-color: ${row3}`"
+        @mouseenter="showR3 = true"
+        @mouseleave="showR3 = false"
+      >
+        <span class="color-hex" v-show="showR3">
+          {{ row1 }}
+        </span>
+      </div>
+      <div
+        class="r4"
+        :style="`background-color: ${row4}`"
+        @mouseenter="showR4 = true"
+        @mouseleave="showR4 = false"
+      >
+        <span class="color-hex" v-show="showR4">
+          {{ row1 }}
+        </span>
+      </div>
     </div>
-    <Button :button="saveButton" v-on:operation="saveColor" />
+    <Button :button="saveButton" v-on:operation="saveColorPallete" />
   </div>
 </template>
 <script>
@@ -27,17 +63,22 @@ export default {
         navigation: "none",
         operation: true,
       },
-    row1:null,
-    row2:null,
-    row3:null,
-    row4:null
+      row1: null,
+      row2: null,
+      row3: null,
+      row4: null,
+      /*  */
+      showR1: false,
+      showR2: false,
+      showR3: false,
+      showR4: false,
     };
   },
-  created(){
-      [this.row1,this.row2,this.row3,this.row4] = [...this.qolor.colors]
+  created() {
+    [this.row1, this.row2, this.row3, this.row4] = [...this.qolor.colors];
   },
   methods: {
-    saveColor() {
+    saveColorPallete() {
       console.log("saving color");
     },
   },
@@ -64,17 +105,36 @@ export default {
 .r1 {
   flex-grow: 3;
   cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 .r2 {
   flex-grow: 2;
   cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 .r3 {
   flex-grow: 1;
   cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 .r4 {
   flex-grow: 1;
   cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.color-hex {
+  background-color: #00000044;
+  color: #fff;
+  padding: 2px;
+  font-family: "lato", sans-serif;
+  font-size: 12px;
 }
 </style>
